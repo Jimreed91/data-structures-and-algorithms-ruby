a4652bb17bdf5a01f2c1540bfb4454d8f6eea8bd
James Reed <jamesreed.3050@gmail.com>
2022-07-19 21:45:59 +0900

    checking speed of my recursive fib method vs regular

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,3 +1,4 @@
+require 'benchmark'
 class Fibonacci
   # non recursive method
   def self.generate(num)
@@ -11,8 +12,8 @@ class Fibonacci
   # recursive fibonacci
   def self.generate_recursively(num, arr = [0, 1])
     # base case
-    return [0] if num < 1
-    return arr if num <= 2
+    return [0] if num <= 1
+    return arr if num == 2
 
     generate_recursively(num - 1, arr << arr[-1] + arr[-2])
   end
@@ -20,3 +21,11 @@ end
 
 p Fibonacci.generate(10)
 p Fibonacci.generate_recursively(10)
+
+# Testing speed (recursive method faster for small amount of numbers but doesnt)
+test_num = 30
+
+Benchmark.bm do |x|
+  x.report('normal') { Fibonacci.generate(test_num) }
+  x.report('recursive') { Fibonacci.generate_recursively(test_num) }
+end
