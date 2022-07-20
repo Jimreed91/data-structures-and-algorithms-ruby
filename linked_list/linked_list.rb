6c9d6e7492c57c9c99b6c8b3f114ced8a6928c98
James Reed <jamesreed.3050@gmail.com>
2022-07-20 23:58:05 +0900

    Working on linked list

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -1,44 +1,47 @@
 require 'pry-byebug'
 class LinkedList
   attr_accessor :head
+  attr_reader :size
+
   def initialize
     @head = nil
+    @size = 0
   end
 
   def append(value)
     if @head.nil?
-      p @head = Node.new(value)
+      @head = Node.new(value)
     else
-      p tail.next_node = Node.new(value)
+      tail.next_node = Node.new(value)
     end
+    @size += 1
   end
-
 
   def prepend(value)
     @head = Node.new(value, @head)
-  end
-
-  def size
-
-  end
-
-  def head
-    return @head.value
+    @size += 1
   end
 
   def tail(node = @head)
     return node if node.next_node.nil?
+
     tail(node.next_node)
+  end
+
+  def at(index, node = @head)
+    return node if index.negative?
+
+    at(index - 1, node.next_node)
   end
 end
 
 class Node
   attr_accessor :next_node, :value
+
   def initialize(value = nil, next_node = nil)
     @next_node = next_node
     @value = value
   end
-
 end
 
 test_list = LinkedList.new
